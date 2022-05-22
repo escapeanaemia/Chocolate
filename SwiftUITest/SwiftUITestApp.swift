26e973c5fe6dbc86284585f4cebbda1508374c08
Andrew <escapeanaemia@gmail.com>
2022-05-22 14:25:38 +0900

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Andrew on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
