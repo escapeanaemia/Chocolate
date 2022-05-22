aef27ee69a87db36b823df32ec2b6ff884fb90a0
Andrew <escapeanaemia@gmail.com>
2022-05-22 21:15:10 +0900

    update

--- SwiftUITestTests/ContentTestsCase.swift
+++ SwiftUITestTests/ContentTestsCase.swift
@@ -26,25 +26,25 @@ class ContentTestsCase: XCTestCase {
     func test검색한_결과가_없을때() throws {
         viewModel.searchText = "ABCDE"
         
-        XCTAssert(viewModel.searchResults.isEmpty)
+        XCTAssert(viewModel.searchResults.isEmpty, "검색 결과가 없을떄는 result가 존재하면 안됩니다. ")
     }
     
     func test검색한_결과가_있을때() throws {
         
         viewModel.searchText = "H"
         
-        XCTAssert(viewModel.searchResults.count == 1 && viewModel.searchResults[0] == testWord)
+        XCTAssert(viewModel.searchResults.count == 1 && viewModel.searchResults[0] == testWord, "검색 결과가 하나만 나와야 하며 Hello가 출력되어야 합니다.")
     }
     
     func test검색한_단어와_일치하는_단어가_없을때() throws {
         viewModel.searchText = "A"
         
-        XCTAssert(viewModel.description.isEmpty)
+        XCTAssert(viewModel.description.isEmpty, "일치하는 단어가 없을 때는 description이 존재하면 안됩니다.")
     }
     
     func test검색한_단어와_일치하는_단어가_있을때() throws {
         viewModel.searchText = testWord
-        XCTAssert(viewModel.description == testWordMeaning)
+        XCTAssert(viewModel.description == testWordMeaning, "일치하는 단어가 있을 때는 description이 존재해야합니다.")
     }
     
 }
